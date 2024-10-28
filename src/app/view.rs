153542ab07b5a0e941ff5d09314e0161aee87a66
dusan <brkicdusan05@gmail.com>
2024-10-28 19:29:06 +0100

    refactor: app view

--- src/app/view.rs
+++ src/app/view.rs
@@ -34,6 +34,99 @@ use super::App;
 
 impl App {
     pub(crate) fn view(&self) -> iced::Element<'_, Message, Theme> {
+        let top_bar = self.view_top();
+
+        let profiles = self.view_profiles();
+
+        let connection_status = self.view_connection();
+
+        let mut content =
+            column![top_bar, Rule::horizontal(0), profiles, connection_status].spacing(10);
+
+        content = content.push_maybe(self.view_error());
+
+        content = content.push_maybe(self.view_success());
+
+        container(content).padding(10).center_x(Length::Fill).into()
+    }
+
+    fn view_profiles(&self) -> iced::widget::Column<'_, Message, Theme> {
+        let mut profiles = column![].align_x(iced::Alignment::Center).spacing(15);
+
+        for p in &self.profiles {
+            profiles = profiles.push(p.get_item(&self.cfg));
+            profiles = profiles.push(Rule::horizontal(0));
+        }
+        profiles
+    }
+
+    fn view_connection(&self) -> iced::widget::Text<'_, Theme> {
+        let connection_status = text(if self.connected {
+            let mut txt = "Connected.".to_string();
+            if self.champion_id.is_some() {
+                txt = format!(
+                    "Selected champion: {}",
+                    get_champion_name_from_id(self.champion_id.unwrap()).unwrap()
+                );
+            }
+            txt
+        } else {
+            let mut txt = "Disconnected.".to_string();
+            if self.retry_in.is_some() {
+                txt = format!(
+                    "{} {} {} seconds...",
+                    txt,
+                    "Retrying in",
+                    self.retry_in.unwrap()
+                );
+            }
+            txt
+        });
+        connection_status
+    }
+
+    fn view_success(&self) -> Option<iced::widget::Container<'_, Message, Theme>> {
+        if let Some(success) = &self.success {
+            let success_text = text(format!("Success! - {}", success))
+                .size(20)
+                .class(theme::Text::Success);
+            let success_container = container(success_text)
+                .class(theme::Container::Success)
+                .center_x(Length::Fill)
+                .center_y(SIZE_LEN);
+            return Some(success_container);
+        }
+        None
+    }
+
+    fn view_error(&self) -> Option<iced::widget::Container<'_, Message, Theme>> {
+        if let Some(e) = &self.error {
+            let error_str = match e {
+                Error::DialogClosed => "Dialog closed without choosing the folder",
+                Error::WrongPath => {
+                    "Wrong path (folder should look like \".../Riot Games/League of Legends/\""
+                }
+                Error::MissingPath => {
+                    "Missing path (choose the League of Legends install directory)"
+                }
+                Error::NameTaken => "Name is taken",
+                Error::ZipExport => "Error exporting profile",
+                Error::ZipImport => "Error importing profile",
+                Error::ChampionTaken => "Another profile already handles that champion",
+            };
+
+            let error_text = text(error_str).size(20).class(theme::Text::Error);
+            let error_container = container(error_text)
+                .class(crate::theme::Container::Error)
+                .center_x(Length::Fill)
+                .center_y(SIZE_LEN);
+
+            return Some(error_container);
+        }
+        None
+    }
+
+    fn view_top(&self) -> iced::widget::Column<'_, Message, Theme> {
         let config_path = text_input("Config not found", self.config.path_to_str()).padding(10);
         let location_btn = tooltip(
             icon_btn(open_icon(), Message::FindLocation.into(), colors::GOLD),
@@ -41,11 +134,6 @@ impl App {
             tooltip::Position::Bottom,
         )
         .class(theme::Container::Tooltip);
-
-        let mut cb = Checkbox::new("Lock settings", self.readonly);
-        if self.cfg.is_some() {
-            cb = cb.on_toggle(Message::SetReadonly)
-        }
 
         let mut add_profile = icon_btn(add_icon(), None, colors::GOLD);
         if self.cfg.is_some() {
@@ -70,6 +158,11 @@ impl App {
             .align_y(iced::Alignment::Center)
             .spacing(10);
 
+        let mut cb = Checkbox::new("Lock settings", self.readonly);
+        if self.cfg.is_some() {
+            cb = cb.on_toggle(Message::SetReadonly)
+        }
+
         let cb = tooltip(
             cb,
             "Settings can't be changed in game while this is active",
@@ -77,73 +170,6 @@ impl App {
         )
         .class(theme::Container::Tooltip);
 
-        let mut profiles = column![].align_x(iced::Alignment::Center).spacing(15);
-
-        for p in &self.profiles {
-            profiles = profiles.push(p.get_item(&self.cfg));
-        }
-
-        let champion_text = text(if self.connected {
-            let mut txt = "Connected.".to_string();
-            if self.champion_id.is_some() {
-                txt = format!(
-                    "Selected champion: {}",
-                    get_champion_name_from_id(self.champion_id.unwrap()).unwrap()
-                );
-            }
-            txt
-        } else {
-            let mut txt = "Disconnected.".to_string();
-            if self.retry_in.is_some() {
-                txt = format!(
-                    "{} {} {} seconds...",
-                    txt,
-                    "Retrying in",
-                    self.retry_in.unwrap()
-                );
-            }
-            txt
-        });
-
-        let mut content =
-            column![location, cb, Rule::horizontal(0), profiles, champion_text].spacing(10);
-
-        if let Some(e) = &self.error {
-            let error_str = match e {
-                Error::DialogClosed => "Dialog closed without choosing the folder",
-                Error::WrongPath => {
-                    "Wrong path (folder should look like \".../Riot Games/League of Legends/\""
-                }
-                Error::MissingPath => {
-                    "Missing path (choose the League of Legends install directory)"
-                }
-                Error::NameTaken => "Name is taken",
-                Error::ZipExport => "Error exporting profile",
-                Error::ZipImport => "Error importing profile",
-                Error::ChampionTaken => "Another profile already handles that champion",
-            };
-
-            let error_text = text(error_str).size(20).class(theme::Text::Error);
-            let error_container = container(error_text)
-                .class(crate::theme::Container::Error)
-                .center_x(Length::Fill)
-                .center_y(SIZE_LEN);
-
-            content = content.push(error_container);
-        }
-
-        if let Some(success) = &self.success {
-            let success_text = text(format!("Success! - {}", success))
-                .size(20)
-                .class(theme::Text::Success);
-            let success_container = container(success_text)
-                .class(theme::Container::Success)
-                .center_x(Length::Fill)
-                .center_y(SIZE_LEN);
-
-            content = content.push(success_container);
-        }
-
-        container(content).padding(10).center_x(Length::Fill).into()
+        column![location, cb].spacing(10)
     }
 }
